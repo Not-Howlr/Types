6dc0c5ce87e963b473c3113014c8055e343830ed
JudeBoy <judegiordano@gmail.com>
2021-09-05 18:54:06 -0400

    Export New Message

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 import * as Shared from "./Types/Shared";
 import * as Constants from "./Types/Constants";
 export import IUser = Shared.IUser;
+export import INewMessage = Shared.INewMessage;
 export import HandlerTypes = Constants.HandlerTypes;
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import * as Shared from "./Types/Shared";
 import * as Constants from "./Types/Constants";
 
 export import IUser = Shared.IUser;
+export import INewMessage = Shared.INewMessage;
 export import HandlerTypes  = Constants.HandlerTypes;
